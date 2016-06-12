77897dbd3ffedad74fac58008afd586b3a96220e
chenxiangdong <1425345959@qq.com>
2016-06-13 00:18:23 +0800

    原始

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(function(){
+  $("#fullpage").fullpage({
+    anchors:["shouye","a","b","c","d","e"],
+    scrollBar:true,
+  })
+})
+var geren=angular.module("geren",[])
+geren.controller("section2",["$scope",function($scope){
+  
+}])
